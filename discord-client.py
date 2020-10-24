fbfc82026168f7036212412d09c0f756c0494863
psmith150 <patricksmith150@gmail.com>
2020-10-24 10:56:10 -0500

    Created basic client

---
+++ discord-client.py
@@ -0,0 +1,37 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    if guild is None:
+        print(f'{client.user} is not a member of {GUILD}: {client.guilds}')
+    else:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if 'Hello' in message.content:
+        await message.channel.send('Greetings!')
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+client.run(TOKEN)
