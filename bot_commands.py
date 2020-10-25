2188f0412855715b58eeb17d28b8043593278719
psmith150 <patricksmith150@gmail.com>
2020-10-25 10:31:33 -0500

    Added home and announce commands

---
+++ bot_commands.py
@@ -0,0 +1,37 @@
+"""Provides commands to be utilized by a Discord bot"""
+from dotenv import load_dotenv
+from requests import get, post
+import os
+import json
+
+def get_home_status(user):
+    """Queries the home status of a Home Assistant user"""
+    load_dotenv()
+    TOKEN = os.getenv('HOME_ASSISTANT_TOKEN')
+    url = f"http://192.168.1.10:8123/api/states/person.{user.lower()}"
+    headers = {
+        'Authorization': f'Bearer {TOKEN}',
+        'content-type': 'application/json',
+    }
+    response = get(url, headers=headers)
+    if response.status_code == 404:
+        return f'User {user} cannot be found.'
+    response_data = response.json()
+    state = response_data.get('state')
+    if state is None:
+        return 'Home state not defined'
+    return f'{user} is currently {state}'
+
+def announce(message):
+    """Announce a message over a speaker"""
+    load_dotenv()
+    TOKEN = os.getenv('HOME_ASSISTANT_TOKEN')
+    url = f"http://192.168.1.10:8123/api/services/tts/google_say"
+    headers = {
+        'Authorization': f'Bearer {TOKEN}',
+        'content-type': 'application/json',
+    }
+    response = post(url, json = {'message':message,'entity_id':'media_player.living_room_speaker'}, headers=headers)
+    if response.status_code != 200:
+        return f'Unable to process message "{message}"'
+    return f'Your message "{message}" was announced!"'
--- discord-client.py
+++ discord-client.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import bot_commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -9,8 +10,29 @@ GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
 bot.case_insensitive = True
 
-@bot.command(name='hello', help='Greets you')
-async def greeting(ctx):
-    await ctx.send('Greetings!')
+@bot.command(name='home')
+async def check_home(ctx, user):
+    """Checks if the specified user is home"""
+    if user is None:
+        await ctx.send('Specify a user to query')
+        return
+    status = bot_commands.get_home_status(user)
+    await ctx.send(status)
+
+@bot.command(name='announce')
+async def announce(ctx, *args):
+    """Sends a message to a speaker"""
+    if (len(args) == 0):
+        await ctx.send("Please enter a message to announce")
+        return
+    message = ' '.join(args)
+    status = bot_commands.announce(message)
+    await ctx.send(status)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if ctx.command.qualified_name == 'home':
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Please specify a user to check')
 
 bot.run(TOKEN)
