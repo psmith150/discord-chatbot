a6f3ce31df184935908ac84bf02f691f9eb0ac97
psmith150 <patricksmith150@gmail.com>
2022-02-12 11:15:59 -0600

    Added looping check for any overdue chores.

--- discord_chatbot/grocy.py
+++ discord_chatbot/grocy.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlite3 import register_converter
 from discord.ext import commands, tasks
 from pygrocy import Grocy as GrocyApp
@@ -9,10 +9,36 @@ class Grocy(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         self.output_channel_id = 769576139516674050
+        self.overdue_chore_last_notified = {}
     
     #region Chores
-    @tasks.loop()
+    @tasks.loop(minutes=1)
     async def notify_chore_overdue(self) -> None:
+        """Checks for any overdue chores and notifies the channel.
+        Notifications will be sent every day until completed.
+        """
+        overdue_chores = get_overdue_chores()
+        chores_to_notify = []
+        notification_delta = timedelta(days=1)
+        for chore in overdue_chores:
+            last_notified = self.overdue_chore_last_notified.get(chore[0], None)
+            if not last_notified or (datetime.now() - last_notified > notification_delta):
+                chores_to_notify.append(chore)
+        
+        # Remove chores from tracking
+        keys_to_remove = []
+        for key in self.overdue_chore_last_notified.keys():
+            match = next(chore for chore in overdue_chores if chore[0] == key)
+            if not match:
+                keys_to_remove.append(key)
+        for key in keys_to_remove:
+            del self.overdue_chore_last_notified[key]
+        
+        channel = self.bot.get_channel(self.output_channel_id)
+        for chore in chores_to_notify:
+            message = f'@here Chore {chore[1]} is overdue.'
+            await channel.send(message)
+            self.overdue_chore_last_notified[chore[0]] = datetime.now()
         pass
 
     @commands.command(name='chore-duedate')
@@ -63,7 +89,7 @@ def get_chore_due_date(name=None) -> 'list[tuple(str, datetime)]':
         queried_chores = chores
     return [(chore.name, chore.next_estimated_execution_time) for chore in queried_chores]
 
-def get_overdue_chores() -> 'list[tuple(str, datetime, str)]':
+def get_overdue_chores() -> 'list[tuple(int, str, datetime, str)]':
     grocy = setup_grocy()
     chores = grocy.chores(True)
     current_datetime = datetime.now()
@@ -75,7 +101,7 @@ def get_overdue_chores() -> 'list[tuple(str, datetime, str)]':
                 user = ''
             else:
                 user = next(user for user in users if user.id == chore.next_execution_assigned_to_user_id)
-            overdue_chores.append((chore.name, chore.next_estimated_execution_time, user))
+            overdue_chores.append((chore.id, chore.name, chore.next_estimated_execution_time, user))
     return overdue_chores
 
 #endregion Stock functions
